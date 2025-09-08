b8f5c05eaf7feb1b6411f286002ce66ad7bfdd0c
seb <sebastientremet.dev@gmail.com>
2025-09-08 12:57:48 +0200

    ✨ Ajout favicon et corrections HTML head

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -6,6 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? SITE_NAME ?></title>
     <link rel="stylesheet" href="/css/main">
+    <!-- Favicon PNG -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+
+    <!-- Favicon ICO -->
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+
+    <!-- Apple Touch Icon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+
+
+
+
 </head>
 
 <body>
@@ -15,7 +28,7 @@
             <div class="header__brand">
                 <span class="header__brand-mark"></span>
                 <a href="/" class="header__link"> <img class="header__logo" src="/images/logo.png" alt="Logo <?= SITE_NAME ?>"></a>
-               
+
             </div>
 
             <button class="header__toggle" aria-label="Menu" aria-expanded="false">
